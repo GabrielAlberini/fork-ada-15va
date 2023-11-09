6f0782f304605f75f478d93fe8dcec5b20c6cb04
GabrielAlberini <77715611+GabrielAlberini@users.noreply.github.com>
2023-11-08 21:31:41 -0300

    Creación de archivo index

---
+++ script/index.js
@@ -0,0 +1,6 @@
+// 1 - Gabo: Tomar un nombre y un apellido por la terminal
+// 2 - Vicky: Crear una función que tome dos argumentos (nombre y apellido). Convertirlo en un módulo.
+// 3 - Escribir lógica de la función para que aparezca en consola asi: ¡HOLA GABRIEL ALBERINI!
+
+const nombre = process.argv[2];
+const apellido = process.argv[3];
